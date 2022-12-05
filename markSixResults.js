b6f4bc294ce7f7b1604d4f1162e0b9903e9af058
Double-X <lkllun@yahoo.com.hk>
2022-12-05 17:28:21 +0800

    Add files via upload

--- markSixResults.js
+++ markSixResults.js
@@ -15,38 +15,27 @@
         ["5", 0],
         ["5.5", 0],
         ["6", 0]
-    ], FREQUENCIES = {}, LEAST_FREQUENT_NUMBERS = new Map();
-    const LEAST_FREQUENT_NUMBER_PRICE_RESULTS = new Map();
-    const LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    const LEAST_FREQUENT_OVER_OLDEST_NUMBERS = new Map();
-    const LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULTS = new Map();
-    const LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    const MIDDLE_FREQUENT_NUMBERS = new Map();
-    const MIDDLE_FREQUENT_NUMBER_PRICE_RESULTS = new Map();
-    const MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    const MOST_FREQUENT_NUMBERS = new Map();
-    const MOST_FREQUENT_NUMBER_PRICE_RESULTS = new Map();
-    const MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    const OLDEST_NUMBERS = new Map(), OLDEST_NUMBER_PRICE_RESULTS = new Map();
-    const OLDEST_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
+    ], FREQUENCIES = {}, NET_GAINS = {}, NUMBERS = {};
+    const NUMBER_PRICE_RESULTS = {}, NUMBER_PRICE_RESULT_COUNTS = {};
     const PRICES = { "4.5": 9600, "4": 640, "3.5": 320, "3": 40 };
-    const RANDOM_NUMBERS = new Map(), RANDOM_NUMBER_PRICE_RESULTS = new Map();
-    const RANDOM_NUMBER_PRICE_RESULT_COUNTS = new Map(
-            FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    const RESULTS = new Map(OLD_RESULTS);
-    const TOTAL_COST = -COST * RESULTS.size, NET_GAINS = {
-        leastFrequentNumbers: TOTAL_COST,
-        leastFrequentOverOldestNumbers: TOTAL_COST,
-        middleFrequentNumbers: TOTAL_COST,
-        mostFrequentNumbers: TOTAL_COST,
-        oldestNumbers: TOTAL_COST,
-        randomNumbers: TOTAL_COST
-    }, collectResults = (result, date) => {
+    const RESULTS = new Map(OLD_RESULTS), STRATEGIES = [
+        "leastFrequent",
+        "leastFrequentMinusOldest",
+        "leastFrequentOverOldest",
+        "leastFrequentPowerOverOldest",
+        "middleFrequent",
+        "mostFrequent",
+        "oldest",
+        "random"
+    ], TOTAL_COST = -COST * RESULTS.size;
+    STRATEGIES.forEach(strategy => {
+        NET_GAINS[strategy] = TOTAL_COST;
+        NUMBERS[strategy] = new Map();
+        NUMBER_PRICE_RESULTS[strategy] = new Map();
+        NUMBER_PRICE_RESULT_COUNTS[strategy] = 
+                new Map(FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
+    });
+    const collectResults = (result, date) => {
         if (Object.keys(FREQUENCIES).length > 0) {
             collectNumbers(date);
             collectPriceResults(date, result);
@@ -55,38 +44,21 @@
         collectAges(numbers);
         collectFrequencies(numbers);
     }, collectNumbers = date => {
-        LEAST_FREQUENT_NUMBERS.set(
+        NUMBERS.leastFrequent.set(
                 date, frequentNumbers(leastFrequentNumberComparator));
-        
-        LEAST_FREQUENT_OVER_OLDEST_NUMBERS.set(
+        NUMBERS.leastFrequentMinusOldest.set(
+                date, leastFrequentMinusOldestNumbers());
+        NUMBERS.leastFrequentOverOldest.set(
                 date, leastFrequentOverOldestNumbers());
-        MIDDLE_FREQUENT_NUMBERS.set(date, middleFrequentNumbers());
-        MOST_FREQUENT_NUMBERS.set(
+        NUMBERS.leastFrequentPowerOverOldest.set(
+                date, leastFrequentPowerOverOldestNumbers());
+        NUMBERS.middleFrequent.set(date, middleFrequentNumbers());
+        NUMBERS.mostFrequent.set(
                 date, frequentNumbers(mostFrequentNumberComparator));
-        OLDEST_NUMBERS.set(date, oldestNumbers());
-        RANDOM_NUMBERS.set(date, randomNumbers());
+        NUMBERS.oldest.set(date, oldestNumbers());
+        NUMBERS.random.set(date, randomNumbers());
     }, collectPriceResults = (date, result) => {
-        collectNumberPriceResults(date, result, LEAST_FREQUENT_NUMBERS,
-                "leastFrequentNumbers", LEAST_FREQUENT_NUMBER_PRICE_RESULTS,
-                LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-        collectNumberPriceResults(date, result, 
-                LEAST_FREQUENT_OVER_OLDEST_NUMBERS,
-                "leastFrequentOverOldestNumbers", 
-                LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULTS,
-                LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS);
-        collectNumberPriceResults(date, result,
-                MIDDLE_FREQUENT_NUMBERS, "middleFrequentNumbers",
-                MIDDLE_FREQUENT_NUMBER_PRICE_RESULTS,
-                MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-        collectNumberPriceResults(date, result, MOST_FREQUENT_NUMBERS,
-                "mostFrequentNumbers", MOST_FREQUENT_NUMBER_PRICE_RESULTS,
-                MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-        collectNumberPriceResults(date, result, OLDEST_NUMBERS,
-                "oldestNumbers", OLDEST_NUMBER_PRICE_RESULTS,
-                OLDEST_NUMBER_PRICE_RESULT_COUNTS);
-        collectNumberPriceResults(date, result, RANDOM_NUMBERS,
-                "randomNumbers", RANDOM_NUMBER_PRICE_RESULTS,
-                RANDOM_NUMBER_PRICE_RESULT_COUNTS);
+        STRATEGIES.forEach(collectNumberPriceResults.bind(null, date, result));
     }, collectAges = numbers => Object.keys(AGES).forEach(number => {
         numbers.includes(number) ? AGES[number] = 0 : AGES[number]++;
     }), collectFrequencies = numbers => numbers.forEach(number => {
@@ -96,10 +68,20 @@
     const mappedFrequentNumbers = ([number]) => number;
     const frequentNumbers = comparator => Object.entries(FREQUENCIES).sort(
             comparator).slice(0, 6).map(mappedFrequentNumbers);
-    const leastFrequentOverOldestNumbers = () => {
+    const leastFrequentMinusOldestNumbers = () => {
+        return Object.entries(FREQUENCIES).map(([number, frequency]) => {
+            const age_ = AGES[number];
+            return [number, age_ ? frequency - age_ : frequency];
+        }).sort(leastFrequentNumberComparator).slice(0, 6).map(mappedFrequentNumbers);
+    }, leastFrequentOverOldestNumbers = () => {
         return Object.entries(FREQUENCIES).map(([number, frequency]) => {
             const age_ = AGES[number];
             return [number, age_ ? frequency * 1.0 / age_ : frequency];
+        }).sort(leastFrequentNumberComparator).slice(0, 6).map(mappedFrequentNumbers);
+    }, leastFrequentPowerOverOldestNumbers = () => {
+        return Object.entries(FREQUENCIES).map(([number, frequency]) => {
+            const age_ = AGES[number];
+            return [number, age_ ? Math.pow(frequency, 1.0 / age_) : frequency];
         }).sort(leastFrequentNumberComparator).slice(0, 6).map(mappedFrequentNumbers);
     }, middleFrequentNumbers = () => {
         const sortedFrequentNumbers = Object.entries(FREQUENCIES).sort(
@@ -127,29 +109,29 @@
             resultantNumbers.push(...allNumbers.splice(randomIndex, 1));
         }
         return resultantNumbers;
-    }, collectNumberPriceResults = (date, result, frequentNumberContainer, netGainType, numberPriceResults, numberPriceResultCounts) => {
-        const numberPriceResult = priceResult(
-                date, result, frequentNumberContainer, netGainType);
-        numberPriceResults.set(date, numberPriceResult);
+    }, collectNumberPriceResults = (date, result, strategy) => {
+        const numberPriceResult = priceResult(date, result, strategy);
+        NUMBER_PRICE_RESULTS[strategy].set(date, numberPriceResult);
         const priceResultCount = `${numberPriceResult}`;
-        numberPriceResultCounts.set(priceResultCount,
-                numberPriceResultCounts.get(priceResultCount) + 1);
-    }, priceResult = (date, result, frequentNumberContainer, netGainType) => {
-        const storedFrequentNumbers = frequentNumberContainer.get(date);
+        const priceResultCounts = NUMBER_PRICE_RESULT_COUNTS[strategy];
+        priceResultCounts.set(
+                priceResultCount, priceResultCounts.get(priceResultCount) + 1);
+    }, priceResult = (date, result, strategy) => {
+        const storedFrequentNumbers = NUMBERS[strategy].get(date);
         const { numbers, prices } = result;
         const normalNumbers = numbers.slice(0, 6);
         const specialNumber = numbers[6];
         const resultCount = normalNumbers.filter(number => {
             return storedFrequentNumbers.includes(number);
         }).length + (storedFrequentNumbers.includes(specialNumber) ? 0.5 : 0);
-        NET_GAINS[netGainType] += (prices[resultCount] || 0) + (
-                PRICES[resultCount] || 0);
+        NET_GAINS[strategy] += 
+                (prices[resultCount] || 0) + (PRICES[resultCount] || 0);
         return resultCount;
-    }, numberPriceResultCountSum = numberPriceResultCounts => [
-        ...numberPriceResultCounts
+    }, numberPriceResultCountSum = strategy => [
+        ...NUMBER_PRICE_RESULT_COUNTS[strategy]
     ].reduce((sum, [count, times]) => sum + +count * times, 0);
-    const numberPositivePriceResultCountSum = numberPriceResultCounts => [
-        ...numberPriceResultCounts
+    const numberPositivePriceResultCountSum = strategy => [
+        ...NUMBER_PRICE_RESULT_COUNTS[strategy]
     ].reduce((sum, [count, times]) => {
         const countNumber = +count;
         return countNumber < 3 ? sum : sum + countNumber * times;
@@ -157,42 +139,29 @@
     RESULTS.forEach(collectResults);
     console.info("RESULTS", RESULTS);
     console.info("FREQUENCIES", FREQUENCIES);
-    console.info("LEAST_FREQUENT_NUMBERS", LEAST_FREQUENT_NUMBERS);
-    console.info("LEAST_FREQUENT_OVER_OLDEST_NUMBERS", LEAST_FREQUENT_OVER_OLDEST_NUMBERS);
-    console.info("MIDDLE_FREQUENT_NUMBERS", MIDDLE_FREQUENT_NUMBERS);
-    console.info("MOST_FREQUENT_NUMBERS", MOST_FREQUENT_NUMBERS);
-    console.info("OLDEST_NUMBERS", OLDEST_NUMBERS);
-    console.info("RANDOM_NUMBERS", RANDOM_NUMBERS);
-    console.info("LEAST_FREQUENT_NUMBER_PRICE_RESULTS", LEAST_FREQUENT_NUMBER_PRICE_RESULTS);
-    console.info("LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULTS", LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULTS);
-    console.info("MIDDLE_FREQUENT_NUMBER_PRICE_RESULTS", MIDDLE_FREQUENT_NUMBER_PRICE_RESULTS);
-    console.info("MOST_FREQUENT_NUMBER_PRICE_RESULTS", MOST_FREQUENT_NUMBER_PRICE_RESULTS);
-    console.info("OLDEST_NUMBER_PRICE_RESULTS", OLDEST_NUMBER_PRICE_RESULTS);
-    console.info("RANDOM_NUMBER_PRICE_RESULTS", RANDOM_NUMBER_PRICE_RESULTS);
-    console.info("LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS", LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS", LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS", MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS", MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("OLDEST_NUMBER_PRICE_RESULT_COUNTS", OLDEST_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("RANDOM_NUMBER_PRICE_RESULT_COUNTS", RANDOM_NUMBER_PRICE_RESULT_COUNTS);
-    console.info("leastFrequentNumberPriceResultCountSum", numberPriceResultCountSum(LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("leastFrequentOverOldestNumberPriceResultCountSum", numberPriceResultCountSum(LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("middleFrequentNumberPriceResultCountSum", numberPriceResultCountSum(MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("mostFrequentNumberPriceResultCountSum", numberPriceResultCountSum(MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("oldestNumberPriceResultCountSum", numberPriceResultCountSum(OLDEST_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("randomNumberPriceResultCountSum", numberPriceResultCountSum(RANDOM_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("leastFrequentNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(LEAST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("leastFrequentOverOldestNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(LEAST_FREQUENT_OVER_OLDEST_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("middleFrequentNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(MIDDLE_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("mostFrequentNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(MOST_FREQUENT_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("oldestNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(OLDEST_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("randomNumberPositivePriceResultCountSum", numberPositivePriceResultCountSum(RANDOM_NUMBER_PRICE_RESULT_COUNTS));
-    console.info("leastFrequentNumbers", frequentNumbers(leastFrequentNumberComparator));
-    console.info("leastFrequentOverOldestNumbers", leastFrequentOverOldestNumbers());
-    console.info("middleFrequentNumbers", middleFrequentNumbers());
-    console.info("mostFrequentNumbers", frequentNumbers(mostFrequentNumberComparator));
-    console.info("oldestNumbers", oldestNumbers());
-    console.info("randomNumbers", randomNumbers());
+    STRATEGIES.forEach(strategy => {
+        console.info(`NUMBERS.${strategy}`, NUMBERS[strategy]);
+    });
+    STRATEGIES.forEach(strategy => {
+        console.info(`NUMBER_PRICE_RESULTS.${strategy}`, NUMBER_PRICE_RESULTS[strategy]);
+    });
+    STRATEGIES.forEach(strategy => {
+        console.info(`NUMBER_PRICE_RESULT_COUNTS.${strategy}`, NUMBER_PRICE_RESULT_COUNTS[strategy]);
+    });
+    STRATEGIES.forEach(strategy => {
+        console.info(`NumberPriceResultCountSum(${strategy})`, numberPriceResultCountSum(strategy));
+    });
+    STRATEGIES.forEach(strategy => {
+        console.info(`NumberPositivePriceResultCountSum(${strategy})`, numberPositivePriceResultCountSum(strategy));
+    });
+    console.info("leastFrequent", frequentNumbers(leastFrequentNumberComparator));
+    console.info("leastFrequentMinusOldest", leastFrequentOverOldestNumbers());
+    console.info("leastFrequentOverOldest", leastFrequentOverOldestNumbers());
+    console.info("leastFrequentPowerOverOldest", leastFrequentPowerOverOldestNumbers());
+    console.info("middleFrequent", middleFrequentNumbers());
+    console.info("mostFrequent", frequentNumbers(mostFrequentNumberComparator));
+    console.info("oldest", oldestNumbers());
+    console.info("random", randomNumbers());
     console.info("TOTAL_COST", TOTAL_COST);
     console.info("NET_GAINS", NET_GAINS);
 })();
