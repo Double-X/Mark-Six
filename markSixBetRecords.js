13b36f0c3fdeb59c8eeac1f0d67ded7feb90676a
Double-X <lkllun@yahoo.com.hk>
2024-04-19 19:24:50 +0800

    Add files via upload

--- markSixBetRecords.js
+++ markSixBetRecords.js
@@ -3904,5 +3904,26 @@ const BET_RECORDS = [
                 strategy: "partitionModuloLeastFrequent"
             }
         }
+    ],
+    [
+        "21/04/2024",
+        {
+            control: {
+                numbers: ["6", "10", "17", "18", "28", "33"],
+                strategy: "random"
+            },
+            mostNetGain: {
+                numbers: ["5", "14", "19", "27", "40", "43"],
+                strategy: "partitionValueLeastFrequent"
+            },
+            mostPositivePriceResultCountSum: {
+                numbers: ["19", "36", "38", "39", "41", "42"],
+                strategy: "partitionModuloLeastFrequent"
+            },
+            mostBiggerPriceResultCountSum: {
+                numbers: ["19", "36", "38", "39", "41", "42"],
+                strategy: "partitionModuloLeastFrequent"
+            }
+        }
     ]
 ];
