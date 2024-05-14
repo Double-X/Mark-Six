cfe441ad3054c1fac679de91bbc35e5021fa3d16
Double-X <lkllun@yahoo.com.hk>
2024-05-14 10:15:10 +0800

    Add files via upload

--- markSixBetRecords.js
+++ markSixBetRecords.js
@@ -4114,5 +4114,26 @@ const BET_RECORDS = [
                 strategy: "partitionModuloLeastFrequent"
             }
         }
+    ],
+    [
+        "14/05/2024",
+        {
+            control: {
+                numbers: ["10", "22", "27", "38", "47", "49"],
+                strategy: "random"
+            },
+            mostNetGain: {
+                numbers: ["5", "14", "19", "27", "40", "43"],
+                strategy: "partitionValueLeastFrequent"
+            },
+            mostPositivePriceResultCountSum: {
+                numbers: ["19", "36", "38", "39", "41", "42"],
+                strategy: "partitionModuloLeastFrequent"
+            },
+            mostBiggerPriceResultCountSum: {
+                numbers: ["19", "36", "38", "39", "41", "42"],
+                strategy: "partitionModuloLeastFrequent"
+            }
+        }
     ]
 ];
